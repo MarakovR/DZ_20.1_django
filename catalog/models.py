0848089f5db4e8b81b5695cd1a2126b5403ae87d
Roman Marakov <marakofr76@yandex.ru>
2023-08-18 11:37:05 +0300

    Создание моделей

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+    picture = models.ImageField(verbose_name='изображение')
+    category = models.ForeignKey('category', on_delete=models.PROTECT, null=True, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена')
+    date_of_creation = models.DateTimeField(verbose_name='дата создания')
+    date_changes = models.DateTimeField(verbose_name='дата изменения')
+
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.product_name} {self.description}'
+
+    class Meta:
+        verbose_name = 'продукт' # Настройка для наименования одного объекта
+        verbose_name_plural = 'продукты' # Настройка для наименования набора объектов
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, verbose_name='категория')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.category_name}'
+
+    class Meta:
+        verbose_name = 'категория' # Настройка для наименования одного объекта
+        verbose_name_plural = 'категории' # Настройка для наименования набора объектов
