d108b1df66092920fbec1827025a2af722e72c4d
Roman Marakov <marakofr76@yandex.ru>
2023-08-21 19:22:24 +0300

    1. Создание нового контроллера и шаблона, которые отвечают за отображение отдельной страницы с товаром 2. Шаблон для главной страницы в виде списка товаров 3. Выделено общий базовый шаблон

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+from catalog.apps import CatalogConfig
+from catalog.views import index, views_contacts, product
 
-from catalog.views import index, views_contacts
-
-app_name = 'catalog'
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', index, name='home'),
+    path('product/', product, name='product'),
     path('contacts/', views_contacts, name='contacts')
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Каталог продуктов'
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def views_contacts(request):
     return render(request, 'catalog/contacts.html')
+
+
+def product(request):
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Продукт'
+    }
+
+    return render(request, 'catalog/product.html', context)
